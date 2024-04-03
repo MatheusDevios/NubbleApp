ee722d39bb0b92bb6d5c5244e1c1180eb6d2096d
Matheus Diniz <matheus.diniiiiz@gmail.com>
2024-04-03 15:28:27 +0100

    Feat: Initiating Module Imports with Absolute Paths and Alias.

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -7,7 +7,7 @@ const SRText = createText<Theme>();
 
 type SRTextProps = React.ComponentProps<typeof SRText>;
 
-interface TextProps extends SRTextProps {
+export interface TextProps extends SRTextProps {
   preset?: TextVariants;
   bold?: boolean;
   italic?: boolean;
--- src/screens/auth/Login/LoginScreen.tsx
+++ src/screens/auth/Login/LoginScreen.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {useForm} from 'react-hook-form';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {Text} from '../../../components/Text/Text';
-import {Button} from '../../../components/Button/Button';
-import {Screen} from '../../../components/Screen/Screen';
+import {
+  Text,
+  Button,
+  Screen,
+  FormTextInput,
+  FormPasswordTextInput,
+} from '@components';
 import {RootStackParamList} from '../../../routes/Routes';
 import {Alert} from 'react-native';
 import {LoginSchema, loginSchema} from './loginSchema';
 import {zodResolver} from '@hookform/resolvers/zod';
-import {FormTextInput} from '../../../components/Form/FormTextInput';
-import {FormPasswordTextInput} from '../../../components/Form/FormPasswordTextInput';
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
 
