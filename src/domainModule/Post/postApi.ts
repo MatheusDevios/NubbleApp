f4beae3dea73bafacd0698a711ec931259827ee2
Matheus Diniz <matheus.diniiiiz@gmail.com>
2024-05-09 21:20:37 +0100

    Feat: integrating API fetch to the app.

--- src/domainModule/Post/postApi.ts
+++ src/domainModule/Post/postApi.ts
@@ -1,11 +1,19 @@
-import {postListMock} from './postListMock';
-import {Post} from './types';
+// import {postListMock} from './postListMock';
+import {Post} from './postTypes';
 
 async function getList(): Promise<Post[]> {
-  // TODO: Simulate API call
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer Mg.iyXo5lxlyKPZSeMWyi4s_rsvoqM73bq1k8bMiLrubMkh9VmKt7RzMYNkj5d_',
+    },
+  });
 
-  await new Promise(resolve => setTimeout(() => resolve(''), 1000));
-  return postListMock;
+  let data = await response.json();
+
+  //await new Promise(resolve => setTimeout(() => resolve(''), 1000));
+  // return postListMock;
 }
 
 export const postApi = {
--- src/domainModule/Post/postTypes.ts
+++ src/domainModule/Post/postTypes.ts
@@ -11,3 +11,5 @@ export interface Post {
   commentCount: number;
   favoriteCount: number;
 }
+
+// export interface PostList {}
