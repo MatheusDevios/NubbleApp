58b9c8051b26d66c1b5266dffb92e6d35664cdc6
Matheus Diniz <matheus.diniiiiz@gmail.com>
2024-05-23 19:42:12 +0100

    Feat: Manipulating data dynamically on the comments with date-fns

--- src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -12,18 +12,13 @@ export function PostCommentItem({postComment}: PostCommentItemProps) {
   return (
     <Box flexDirection="row" mb="s16" alignItems="center">
       <ProfileAvatar imageURL={postComment.author.profileURL} />
-      <Box ml="s12">
+      <Box ml="s12" flex={1}>
         <Text preset="paragraphSmall" bold>
           {postComment.author.userName}
         </Text>
-        <Box flexDirection="row">
-          <Text preset="paragraphSmall" color="gray1">
-            {postComment.message}
-          </Text>
-          <Text preset="paragraphSmall" color="gray1">
-            - {postComment.createdAt}
-          </Text>
-        </Box>
+        <Text preset="paragraphSmall" color="gray1">
+          {postComment.message} - {postComment.createdAtRelative}
+        </Text>
       </Box>
     </Box>
   );
---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,40 @@
+import {format, parseISO, differenceInSeconds} from 'date-fns';
+
+const formateRelative = (dateISO: string): string => {
+  const date = parseISO(dateISO);
+  const now = new Date();
+
+  const diffInSeconds = differenceInSeconds(now, date);
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} sec`;
+  }
+
+  const diffInMinutes = Math.round(diffInSeconds / 60);
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes} mim`;
+  }
+
+  const diffInHours = Math.round(diffInMinutes / 60);
+  if (diffInHours < 24) {
+    return `${diffInHours} h`;
+  }
+
+  const diffInDays = Math.round(diffInHours / 24);
+  if (diffInDays < 30) {
+    return `${diffInDays} d`;
+  }
+
+  const diffInWeeks = Math.round(diffInDays / 7);
+  if (diffInWeeks < 4) {
+    return `${diffInWeeks} w`;
+  }
+
+  const diffInMonths = Math.round(diffInDays / 30);
+  if (diffInMonths < 12) {
+    return `${diffInMonths} m`;
+  }
+
+  return format(date, 'dd/MM/yyyy');
+};
+
+export const dateUtils = {formateRelative};
