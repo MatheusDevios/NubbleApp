e2792d42a860aed60ed614d3d4bf8f521bfca298
Matheus Diniz <matheus.diniiiiz@gmail.com>
2024-05-26 22:49:03 +0100

    Feat: Added a simple animation to the Toast Notifier.

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,6 +1,8 @@
-import React, {useEffect} from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useCallback, useEffect, useRef} from 'react';
+import {Animated} from 'react-native';
 
-import {useToast, useToastService} from '@services';
+import {ToastPosition, useToast, useToastService} from '@services';
 
 import {ToastContent} from './components/ToastContent';
 
@@ -10,17 +12,52 @@ export function Toast() {
   const toast = useToast();
   const {hideToast} = useToastService();
 
+  const position: ToastPosition = toast?.position || 'top';
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const runEntranceAnimation = useCallback(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  const runExitAnimation = useCallback(
+    (callback: Animated.EndCallback) => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start(callback);
+    },
+    [fadeAnim],
+  );
+
   useEffect(() => {
     if (toast) {
+      runEntranceAnimation();
+
       setTimeout(() => {
-        hideToast();
+        runExitAnimation(hideToast);
       }, toast.duration || DEFAULT_DURATION);
     }
-  }, [hideToast, toast]);
+  }, [hideToast, runEntranceAnimation, runExitAnimation, toast]);
 
   if (!toast) {
     return null;
   }
 
-  return <ToastContent toast={toast} />;
+  return (
+    <Animated.View
+      style={{
+        position: 'absolute',
+        alignSelf: 'center',
+        opacity: fadeAnim,
+        [position]: 100,
+      }}>
+      <ToastContent toast={toast} />
+    </Animated.View>
+  );
 }
--- src/components/Toast/components/ToastContent.tsx
+++ src/components/Toast/components/ToastContent.tsx
@@ -2,7 +2,7 @@
 import React from 'react'; // Import React
 import {Dimensions} from 'react-native';
 
-import {Toast, ToastPosition, ToastType} from '@services';
+import {Toast, ToastType} from '@services';
 
 import {Box, BoxProps, Icon, IconProps, Text} from '@components';
 import {$shadowProps} from '@theme';
@@ -14,11 +14,10 @@ interface Props {
 }
 
 export function ToastContent({toast}: Props) {
-  const position: ToastPosition = toast?.position || 'top';
   const type: ToastType = toast?.type || 'success';
 
   return (
-    <Box {...$boxStyle} style={[{[position]: 100}, $shadowProps]}>
+    <Box {...$boxStyle} style={$shadowProps}>
       <Icon {...mapTypeToIcon[type]} />
       <Text style={{flexShrink: 1}} ml="s16" preset="paragraphMedium" bold>
         {toast?.message}
@@ -39,9 +38,7 @@ const mapTypeToIcon: Record<ToastType, IconProps> = {
 };
 
 const $boxStyle: BoxProps = {
-  position: 'absolute',
   backgroundColor: 'background',
-  alignSelf: 'center',
   alignItems: 'center',
   padding: 's16',
   borderRadius: 's16',
