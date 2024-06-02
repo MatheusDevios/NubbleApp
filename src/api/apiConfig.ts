7c58079c8d1fe2ff9f3b6d5a39dc53a5e8248276
Matheus Diniz <matheus.diniiiiz@gmail.com>
2024-06-02 20:37:50 +0100

    Config: Configuring authToken on Axios instance.

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,8 +2,4 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
-  headers: {
-    Authorization:
-      'Bearer NA.b4n8C1Up7xDyyxYqMoQlYMJPRUNGuzS51nK80vHovSBXqQe8q3QWACoK7-ke',
-  },
 });
