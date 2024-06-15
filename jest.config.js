0d10def34cb8f20dbb55a57d000d33f7b2296427
Matheus Diniz <matheus.diniiiiz@gmail.com>
2024-06-15 19:16:28 +0100

    Config: Initial Jest test config.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   preset: 'react-native',
+  // collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'index'],
 };
