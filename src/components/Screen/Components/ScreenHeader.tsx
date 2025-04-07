44465fb1ec945ce1bdc344393e49544dec67c977
Matheus Diniz <matheus.diniiiiz@gmail.com>
2025-04-07 19:49:37 +0100

    style

--- src/components/Screen/Components/ScreenHeader.tsx
+++ src/components/Screen/Components/ScreenHeader.tsx
@@ -30,7 +30,7 @@ export function ScreenHeader({
           onPress={() => navigation.goBack()}
           flexDirection="row"
           alignItems="center"
-          mr="s10">
+          mr="s8">
           <Icon size={ICON_SIZE} name="arrowLeft" color="primary" />
           {showBackLabel && (
             <Text preset="paragraphMedium" semiBold ml="s8">
